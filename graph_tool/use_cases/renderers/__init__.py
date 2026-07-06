e3d0d70578e4830aa4be490e84f1da014c5671cf
BobRobertNew <guy.victor@gmail.com>
2026-07-06 09:25:05 +0200

    Remove DatashaderRenderer from exports
    Comment out the import and declaration of DatashaderRenderer.

--- graph_tool/use_cases/renderers/__init__.py
+++ graph_tool/use_cases/renderers/__init__.py
@@ -1,5 +1,5 @@
 from .base import GraphRenderer
 from .pyvis_renderer import PyVisRenderer
-from .datashader_renderer import DatashaderRenderer
+#from .datashader_renderer import DatashaderRenderer
 
-__all__ = ["GraphRenderer", "PyVisRenderer", "DatashaderRenderer"]
+__all__ = ["GraphRenderer", "PyVisRenderer"] #, "DatashaderRenderer"]
